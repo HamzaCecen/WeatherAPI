7ee8f9e97a789d13da027a925186b86a27289d29
Hamza <93983281+HamzaCecen@users.noreply.github.com>
2022-09-27 14:54:32 +0300

    Add project files.

---
+++ Weather/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Weather
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            string api = "da2f5c3711ebaece9ca89418e8969159";
+            string connection = "http://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument weather = XDocument.Load(connection);
+            var temp = weather.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var weatherstate = weather.Descendants("weather").ElementAt(0).Attribute("value");
+            Console.Write("İstanbul için sıcaklık: " + temp + "-- hava durum: " + weatherstate);
+            Console.ReadLine();
+        }
+    }
+}

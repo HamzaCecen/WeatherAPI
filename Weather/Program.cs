1b11960d63ae16cff5d24170ce4eabb99cc25b9e
Hamza <93983281+HamzaCecen@users.noreply.github.com>
2022-09-27 14:57:22 +0300

    Value name changed

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -11,8 +11,8 @@ namespace Weather
     {
         static void Main(string [] args)
         {
-            string api = "da2f5c3711ebaece9ca89418e8969159";
-            string connection = "http://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            string API = "da2f5c3711ebaece9ca89418e8969159";
+            string connection = "http://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + API;
             XDocument weather = XDocument.Load(connection);
             var temp = weather.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             var weatherstate = weather.Descendants("weather").ElementAt(0).Attribute("value");
